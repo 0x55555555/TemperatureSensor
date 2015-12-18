756e99c5817c60d3a523ee08e08d05b2ee4714be
Jorj Pimm <jorjpimm@gmail.com>
2015-12-18 16:14:52 +0000

    initial stab at temperature logging

---
+++ Monitor.py
@@ -0,0 +1,26 @@
+
+def read_sensor(name):
+    f = open(name)
+    out = f.read().split("\n")
+    f.close()
+    return float(out[1].split(" ")[9][2:])/1000
+
+sensors = [
+    "/sys/bus/w1/devices/28-000007215308/w1_slave",
+    "/sys/bus/w1/devices/28-000007481742/w1_slave",
+    "/sys/bus/w1/devices/28-00000748446c/w1_slave"
+]
+
+import time, json, datetime, calendar
+
+with open('out.txt', 'w') as file:
+    while True:
+        temps = {
+            'time': time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()),
+            'epoch': calendar.timegm(time.gmtime()),
+            'temps': [read_sensor(s) for s in sensors]
+        }
+        file.write(json.dumps(temps) + ',\n')
+        file.flush()
+        print("Flushed to file", temps)
+        time.sleep(60)
