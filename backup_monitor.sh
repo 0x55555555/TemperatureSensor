65761850591744da1d9b95ff2b5d2e1883ea2c14
Jorj Pimm <jorjpimm@gmail.com>
2015-12-20 22:23:28 +0000

    fiddling with tun scripts

--- backup_monitor.sh
+++ backup_monitor.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SOURCE=$DIR/live_output.json
+SOURCE=$DIR/data/live_output.json
 DATE=$(date +"%Y_%m_%d_%H_%M")
 DEST_NAME="output_$DATE.json"
 DEST=/media/temperature_data/$DEST_NAME
 
-cp $SOURCE $DIR/$DEST_NAME
+cp $SOURCE $DIR/data/$DEST_NAME
 cp $SOURCE $DEST 
 if [ -f $dest ];
 then
--- run_monitor.sh
+++ run_monitor.sh
@@ -3,4 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
-python ./Monitor.py $DIR/live_output.json
+
+(cd web ; node server.js ../data/ &)
+
+python ./Monitor.py $DIR/data/live_output.json
