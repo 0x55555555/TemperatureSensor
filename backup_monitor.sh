17c78db070509ef3734f8c32e79e315702478d54
Jorj Pimm <jorjpimm@gmail.com>
2015-12-20 09:03:16 +0000

    adding helper scripts

---
+++ backup_monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SOURCE=$DIR/live_output.json
+DATE=$(date +"%Y_%m_%d_%H_%M")
+DEST_NAME="output_$DATE.json"
+DEST=/media/temperature_data/$DEST_NAME
+
+cp $SOURCE $DIR/$DEST_NAME
+cp $SOURCE $DEST 
+if [ -f $dest ];
+then
+    rm $SOURCE
+fi
